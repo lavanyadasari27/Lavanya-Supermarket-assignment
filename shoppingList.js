3a6a2dd9ca5e0402f1bb2df6c071d0e6363b6f58
lavanyadasari <lavanya.dasari@everest.engineering>
2023-08-15 23:35:03 +0530

    Details of the item i.e, name, quantity, measurement and Price is printed

--- shoppingList.js
+++ shoppingList.js
@@ -3,17 +3,44 @@ const fs = require("fs");
 const jsonString = fs.readFileSync("./shoppingList.json", "utf-8");
 const jsonData = JSON.parse(jsonString);
 
-
 function UserDetails(user) {
   return `
-    <p><strong>Store Name:</strong> ${user.storename}</p>
-    <p><strong>Address:</strong> ${user.address}</p>
-    <p><strong>Phone Number:</strong> ${user["Phone number"]}</p>`;
+    <p><strong></strong> ${user.storename}</p>
+    <p><strong></strong> ${user.address}</p>
+    <p>GST NO:${user["GST NO"]}, PH:${user["Phone number"]}</p>`;
 }
 
 
+function ShoppingListDetails(shoppingList) {
+
+  let shoppingListDescription = `
+  <div>
+  <div>
+  ***************************** TAX INVOICE ******************************
+  </div>
+  <div><th>Name</th>
+  <th>Quantity</th>
+  <th>Measurement</th>
+  <th>Category</th>
+  <p>---------------------------------------------------------------------</p>
+  </div>
+  </div>`;
+    shoppingList.forEach((item) => {
+    
+    shoppingListDescription += ` 
+    <div>
+    <p>${item.name} ${item.quantity} ${item.measurement} ${item.Price}  </p>
+    <p>--------------------------------------------------------------------</p>
+    </div>`;
+  });
+
+  
+  return shoppingListDescription;
+}
+
 function generateHTML(data) {
   const userDetailsHTML = UserDetails(data.user);
+  const shoppingListHTML = ShoppingListDetails(data.shoppingList);
 
   return `
     <!DOCTYPE html>
@@ -24,11 +51,20 @@ function generateHTML(data) {
       <title>Shopping List</title>
     </head>
     <body>
+    <div>
       <h1>User Information</h1>
       ${userDetailsHTML}
       
       <h1>Shopping List</h1>
-       
+       ${shoppingListHTML}
+    </div>
+
+    <div>
+    For Queries & Complaints 
+    WhatsApp using 9100811811
+    <br>www.facebook.com/ratnadeepsupermarket
+    
+    </div>
     </body>
     </html>`;
 }
