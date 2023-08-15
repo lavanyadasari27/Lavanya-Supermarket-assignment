f9e0dcf21f79bba7d15ccd4075731c5c4d5af67c
lavanyadasari <lavanya.dasari@everest.engineering>
2023-08-15 13:02:15 +0530

    Added HTML structure for displaying User details and Shopping List details.

--- shoppingList.js
+++ shoppingList.js
@@ -5,5 +5,27 @@ const jsonData = JSON.parse(jsonString);
 
 
 
+function generateHTML(data) {
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Shopping List</title>
+    </head>
+    <body>
+      <h1>User Information</h1>
+      
+      <h1>Shopping List</h1>
+       
+    </body>
+    </html>`;
+}
+
+
+
+
 const htmlContent = generateHTML(jsonData);
 fs.writeFileSync("./shoppingList.html", htmlContent);
