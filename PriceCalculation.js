07724874a055410d3676a1ba445c3ea20843f278
naveenavusali18 <naveen.avusali@everest.engineering>
2023-08-16 23:15:38 +0530

    added store level discount on items original price

--- PriceCalculation.js
+++ PriceCalculation.js
@@ -1,8 +1,10 @@
-const itemGST=require("./ItemGST");
+const itemGST = require("./ItemGST");
 
 function ShoppingListDetails(shoppingList) {
   let Total = 0;
   let grandTotal = 0;
+  let totalGST = 0;
+  let discount = 0;
 
   let shoppingListDescription = `
   <div id="cart_details">
@@ -20,18 +22,15 @@ function ShoppingListDetails(shoppingList) {
   <th>Quantity</th>
   <th>Measurement</th>
   <th>Price</th>
-  <th>Price(Includes GST)</th>
+  <th>GST</th>
   </strong>
    </tr>
   </div>
   </div>`;
   shoppingList.forEach((item) => {
     const gstPrice = itemGST(item);
-    const itemPriceWithGst = item.Price + gstPrice; ////Price of item including Gst for 1 quantity
-    const itemAmountWithGST = itemPriceWithGst * item.quantity;
-
+    totalGST += gstPrice * item.quantity;
     Total += item.Price * item.quantity;
-    grandTotal += itemAmountWithGST;
     shoppingListDescription += ` 
     <div id="items">
     <div id="list">
@@ -40,12 +39,22 @@ function ShoppingListDetails(shoppingList) {
     <td> ${item.quantity}</td>
      <td>${item.measurement}</td>
      <td>${item.Price}</td>
-     <td>${itemAmountWithGST.toFixed(2)}</td>
+     <td>${gstPrice * item.quantity}</td>
     </tr>
     </div>
     </div>
     `;
   });
+  if (Total >= 500 && Total < 1000) {
+    discount = Total * 0.05;
+  } else if (Total >= 1000 && Total < 1500) {
+    discount = Total * 0.1;
+  } else if (Total >= 1500 && Total < 2000) {
+    discount = Total * 0.15;
+  } else {
+    discount = Total * 0.2;
+  }
+  grandTotal = Total - discount + totalGST;
   shoppingListDescription += ` 
   </table>
   <div id="alignment">
@@ -53,7 +62,9 @@ function ShoppingListDetails(shoppingList) {
     </div>
     <div id="TotalAmount">
     <strong>Total: ${Total.toFixed(2)}</strong>
-    <br><strong>Grand Total: ${grandTotal.toFixed(2)}</strong>
+    <br><strong>Discount: -${discount}</strong><br>
+    <strong>GST: +${totalGST}</strong><br>
+    <strong>Grand Total: ${grandTotal.toFixed(2)}</strong>
     <br>---------------------------------------------------------------------
     </div>`;
 
